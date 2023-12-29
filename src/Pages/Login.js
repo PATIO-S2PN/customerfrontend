25b4e4f3f00d5a0772c378a23744ee9e34d401ef
Supuni Jayasinghe <139542877+SupuniJayasinghe@users.noreply.github.com>
2023-12-29 12:18:18 +0530

    creating the register button in the login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -19,10 +19,10 @@ function Login() {
       </div>
       
       <div className='w-full md:w-1/2 h-half md:h-full bg-[#101E21] flex flex-col p-custom'>
-        
         <div className='justify-center'>
             <h1 className='font-serif text-3xl text-[#FFFFFF] text-center mb-10 mt-20'>User Login</h1>
         </div>
+
         <div className='flex justify-center mb-4 space-x-5'>
             <a href="#" className="cursor-pointer">
                     <FcGoogle size={30} />
@@ -38,9 +38,9 @@ function Login() {
                  fill="currentColor" viewBox="0 0 24 24">
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
               </svg>
-            </a>
-                     
+            </a>             
         </div>
+
         <div className="flex items-center mb-4 space-x-4 text-white">
             <hr className="flex-grow border-white"/>
             <span className='font-serif'>or</span>
@@ -48,10 +48,11 @@ function Login() {
         </div>
         
         <input type='text' className='mb-8 text-sm font-serif p-2.5 text-white bg-slate-800 rounded-lg h-10 border-b-2 border-[#FADBCF] focus:outline-none'  placeholder='Email*' required/>        
+        
         <div className='relative'>
           <input
             type={showPassword ? 'text' : 'password'}
-            className='mb-2 text-sm font-serif p-2.5 text-white bg-slate-800 rounded-lg h-10 border-b-2 w-full border-[#FADBCF] focus:outline-none'
+            className='mb-3 text-sm font-serif p-2.5 text-white bg-slate-800 rounded-lg h-10 border-b-2 w-full border-[#FADBCF] focus:outline-none'
             placeholder='Password*'
             required
           />
@@ -61,19 +62,24 @@ function Login() {
             onClick={handleToggle}
           />
         </div>
-        
-        <a href='#' className='text-[#1889F2] font-serif text-sm mb-12'>Forgot Password?</a>   
+
+        <div className='flex justify-between items-center space-x-3 space-y-0 mb-7'>
+            <div className='flex items-center space-x-2'>
+                <input type="checkbox" className="w-4 h-4 text-blue-600 border border-black form-checkbox" />
+                <p className='text-[#E3D0D0] font-serif text-sm'>Remember me</p>
+            </div>
+            <a href='#' className='text-[#1889F2] font-serif text-sm mb-12'>Forgot Password?</a>   
+        </div>
 
         <div className='flex justify-center '>
-            <button className='text-xl text-white bg-[#854703] rounded-lg h-10 w-60 justify-center mb-6'>Login</button>   
+            <button className='text-xl text-white bg-[#854703] rounded-lg h-10 w-60 justify-center mb-10'>Login</button>   
         </div>
              
         <div className='flex items-center space-x-2'>
-            <input type="checkbox" className="w-4 h-4 text-blue-600 border border-black form-checkbox" />
-            <p className='text-[#E3D0D0] font-serif'>Remember me</p>
+            <p className='text-[#E3D0D0] font-serif'>Don't have an account yet?</p>
+            <a href='#' className='text-[#1889F2] font-serif ml-2'>Create Account</a>
         </div>
-
-        
+ 
     </div>
   </div>
   )
