3ee9212e39a96a89465cd7971f5df76b74a94890
SupuniJayasinghe <supuninavodya695@gmail.com>
2024-06-04 22:08:48 +0530

    login is woking for "Enter" key

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -42,6 +42,12 @@ function Login() {
   const [password, setPassword] = useState('');
   const [loginMessage, setLoginMessage] = useState('');
 
+  const handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      handleLoginClick();
+    }
+  }
+
   const handleLoginClick = async () => {
     try {
       const response = await axios.post('http://34.224.26.99/customer/login', {
@@ -120,6 +126,7 @@ function Login() {
         <input type='text' 
                className='mb-8 text-sm font-serif p-2.5 text-white bg-slate-800 rounded-lg h-10 border-b-2 border-[#FADBCF] focus:outline-none'  
                placeholder='Email*' required
+               onKeyPress={handleKeyPress}
                onChange={(e) => setEmail(e.target.value)}/>        
         
         <div className='relative'>
@@ -128,6 +135,7 @@ function Login() {
             className='mb-3 text-sm font-serif p-2.5 text-white bg-slate-800 rounded-lg h-10 border-b-2 w-full border-[#FADBCF] focus:outline-none'
             placeholder='Password*'
             required
+            onKeyPress={handleKeyPress}
             onChange={(e) => setPassword(e.target.value)}       
 
           />
